8d6dc096227db7a5baa188d740478d9f7be261f8
Numan <numanhaider29@yahoo.com>
2023-09-29 13:32:14 -0700

    04_comparisons explained with comments

---
+++ 04_comparison.js
@@ -0,0 +1,19 @@
+// console.log(2>1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+// console.log(2>=1)
+
+
+//console.log("2">1) these types of comparisons should be avoided
+//console.log("02">1)
+
+console.log(null < 0) //false
+console.log(null > 0) //false
+console.log(null >= 0) // true null is converted to zero
+
+console.log(undefined < 0)
+
+// === this is triple equal sighn also checks the data types 
+
+console.log("2"===2) // false because "2" is string and 2 is a number
