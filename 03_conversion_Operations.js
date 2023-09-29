29174a6bbfbf074784d4139f2df9c426e93a04f4
Numan <numanhaider29@yahoo.com>
2023-09-29 13:19:16 -0700

    Conversions finalized

--- 03_conversion_Operations.js
+++ 03_conversion_Operations.js
@@ -16,5 +16,24 @@ let booleanIsLoggedIn = Boolean(loggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
+
+
+// ************Operations*********
+let value = 3
+let negValue = -value;
+//console.log(-value)
+//console.log("1"+2)
+//console.log(1+"2")
+//console.log("1"+2+2)
+
+//console.log(1+2+"2")
+//console.log(+true) this is a just foolish to write such code
+
+let num1,num2,num3
+num1 = num2 = num3= 20+23
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
