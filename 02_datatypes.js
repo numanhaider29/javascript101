00dfe18b0a3a05c729d419f562839a095d9cfb40
Numan <numanhaider29@yahoo.com>
2023-09-29 12:55:37 -0700

    03_Conversions

---
+++ 02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; // use all javascript code as newer version
+//alert(3+3) we are using node js 
+console.log("object")
+console.log(3+30)// code readability should be high
+
+let age = 18
+// number range 2 to power 53
+// null is a standalone value or a type
+// undefined 
+// symbol => unique
+
+// object 
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
---
+++ 03_conversion_Operations.js
@@ -0,0 +1,20 @@
+let score = "3numan"
+//console.log( typeof score)
+//console.log( typeof (score))
+let valueInNumber = Number(score)
+//console.log(typeof(valueInNumber))
+//console.log(valueInNumber)
+
+let loggedIn = "asad"
+let booleanIsLoggedIn = Boolean(loggedIn)
+//console.log(booleanIsLoggedIn)
+
+// 1 true
+// 0 false
+// "asdasd" true
+// "" false
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
